73324cdd97e5c8fee869aefe5f5315b6c06cb76d
monstermario <mw.topdev@gmail.com>
2025-03-13 15:10:04 +0200

    seed database with test user

--- src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,5 +1,9 @@
 package com.example.currencyconversionservice;
 
+import com.example.currencyconversionservice.model.User;
+import com.example.currencyconversionservice.respository.UserRepository;
+import com.example.currencyconversionservice.service.ApiKeyGenerator;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,5 +19,18 @@ public class CurrencyConversionServiceApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository userRepository) {
+        return args -> {
+            if (userRepository.count() == 0) {
+                User user = new User();
+                user.setApiKey(ApiKeyGenerator.generateApiKey());
+                user.setName("Test User");
+                userRepository.save(user);
+                System.out.println("Generated API Key: " + user.getApiKey());
+            }
+        };
     }
 }
